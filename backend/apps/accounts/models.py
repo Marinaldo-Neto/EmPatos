ca1ee8fb67c08394ac2fafaa46f30b02d3e1b0c5
Auceptin <ramalhotiago644@gmail.com>
2026-03-28 07:58:55 -0300

    feat: cria model de user

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,2 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        PROVIDER = "PROVIDER", "Prestador"
+        CLIENT = "CLIENT", "Cliente"
+
+    username = None
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.CLIENT
+        )
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
